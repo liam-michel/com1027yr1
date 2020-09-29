89bfc865d732e6313b5f33f0b69dc4e23a8c53a8
liamd <liamd@DESKTOP-EGTUQRD.surrey.ac.uk>
2020-09-29 20:01:54 +0100

    New changed hello world code

---
+++ Lab01/exercise1/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+package exercise1;
+
+public class HelloWorld {
+
+	public void main() {
+		System.out.println(helloWorld());
+	}
+
+	public static String helloWorld() {
+		//add your code below thus this helloWorld method returns "Hello World!"
+		return "Hello World!";
+
+		
+	}
+
+}
